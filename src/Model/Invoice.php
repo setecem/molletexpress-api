eb59dcf4dc804fd45b7ca9fdbc0c52d63d39775c
programacion3 <setecem.programacion3@gmail.com>
2025-11-19 12:56:12 +0100

    FIX invoice

--- src/Model/Invoice.php
+++ src/Model/Invoice.php
@@ -15,7 +15,7 @@ class Invoice extends Model
     public ?Client $client = null;
     public ?string $comment = null;
     public int $subtotal = 0;
-    public ?DateTime $date = null;
+    public DateTime|string|null $date = null;
 
 
     // Clonados de contact
